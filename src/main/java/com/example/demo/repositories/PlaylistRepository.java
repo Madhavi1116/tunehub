e88b96d96f07256820014281b291e06f982463a0
Shiva <Shiva@LAPTOP-ONPRK6M0>
2024-02-05 12:11:51 +0530

    1st version of tune hub

---
+++ src/main/java/com/example/demo/repositories/PlaylistRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Playlist;
+
+import com.example.demo.entity.Playlist;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Integer>{
+
+}
